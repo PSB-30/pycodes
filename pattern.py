b1eec0e8efe2b5f4a55ddbd6c63ee062da0c57f4
Swapnesh Pawar <104996733+Swapvp@users.noreply.github.com>
2024-11-26 20:14:00 +0530

    Enhancements pattern.py
    1.User Input: Allows for flexibility in choosing height and pattern.
    
    2 Custom Characters: You can use stars, hashtags, or even alphabets for the pyramid.
    
    3. Clear Alignment: Maintains a visually appealing structure regardless of height.

--- pattern.py
+++ pattern.py
@@ -1,9 +1,25 @@
 #scope of this project is to print the pyramid patten
 
-n=5
-for i in range(n):
-    for j in range(n-i-1):
-        print(" ",end='')
-    for j in range(i+1):
-        print("*",end=" ")
-    print(" ")
+# n=5
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print(" ",end='')
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print(" ")
+
+# Pyramid Pattern with Customization
+def print_pyramid():
+    # Get user input for the height and character
+    n = int(input("Enter the height of the pyramid: "))
+    char = input("Enter the character for the pyramid: ")
+
+    print("\nPyramid Pattern:\n")
+    for i in range(n):
+        # Print leading spaces
+        print(" " * (n - i - 1), end='')
+        # Print pyramid characters
+        print((char + " ") * (i + 1))
+        
+# Call the function
+print_pyramid()
